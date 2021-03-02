1eddbf1631252948714bd261bf1f695b0eab1fc1
Sergio Octavio <sergio.sanmartinf@gmail.com>
2021-03-02 14:51:15 -0300

    One change

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { AppRouter } from './router/AppRouter'
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
 
 export const CalendarApp = () => {
     return (
         <div>
-            <AppRouter/>
+            <AppRouter />
         </div>
-    )
+    );
 }
